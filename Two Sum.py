ed63cbf2f265b21566a3077a7af366c43d9c0505
Asequr Rahman <asiq13096@gmail.com>
2023-02-14 19:45:13 +0600

    1st Solution

---
+++ Two Sum.py
@@ -0,0 +1,22 @@
+def twosum(nums, target):
+    indices = []
+    a = len(nums)
+    for i in range(a):
+        for j in range(i + 1, a):
+            if nums[i] + nums[j] == target:
+                for index, value in enumerate(nums):
+                    if index == j:
+                        indices.append(index)
+                    elif index == i:
+                        indices.append(index)
+    return indices
+
+
+num = []
+number = int(input("Target Number: "))
+length = int(input("Number of elements: "))
+for n in range(0, length):
+    element = int(input())
+    num.append(element)
+another = twosum(num, number)
+print(another)
